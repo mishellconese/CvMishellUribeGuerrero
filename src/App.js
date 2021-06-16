e8c83f88b4e20a82c68a78eefaf6442ca9134cf9
mishell uribe <mishelluribe0987@gmail.com>
2021-06-15 19:36:44 -0500

    cambios del 99%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import logo from './utils/images/logo.svg';
 import './App.css';
+import GlobalStyles from './GlobalStyle.js'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from "./pages/Home/Home.jsx"
+import Contact from "./pages/Contact/Contact.jsx"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+	<GlobalStyles />
+	<Switch>
+	<Route path="/" exact component={Home} />
+	<Route path="/contact" exact component={Contact} />
+	</Switch>
+	</Router>
+      
     </div>
   );
 }
